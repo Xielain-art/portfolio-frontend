79cbd844f82aacab9cca8ce03b13a680b9a3d7bc
Xielain <rklimow409@gmail.com>
2023-07-10 14:52:29 +0300

    generated header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+  standalone: true,
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
