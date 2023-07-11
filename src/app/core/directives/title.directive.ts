0f6dfd26c8b59a3b8184a09fcd3cd7d4f7e05e2e
Xielain <rklimow409@gmail.com>
2023-07-11 14:34:45 +0300

    changed title directive

--- src/app/core/directives/title.directive.ts
+++ src/app/core/directives/title.directive.ts
@@ -1,17 +1,32 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
   selector: '[appTitle]',
 })
-export class TitleDirective {
+export class TitleDirective implements OnChanges {
   @Input() fontSizeType: 'h1' | 'h2' = 'h1';
 
   private domElement: HTMLElement;
 
   constructor(private elemRef: ElementRef) {
     this.domElement = this.elemRef.nativeElement;
-    this.fontSizeType === 'h1'
-      ? this.domElement.classList.add('title')
-      : this.domElement.classList.add('title', 'title2');
+    this.init();
+  }
+
+  private init(): void {
+    this.domElement.classList.add('title');
+    return;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['fontSizeType']) {
+      this.domElement.classList.add('title2');
+    }
   }
 }
