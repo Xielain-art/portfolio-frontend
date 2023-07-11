a5c6854bd346b8d03c445b618f2c0b2420cf50a5
Xielain <rklimow409@gmail.com>
2023-07-11 17:06:47 +0300

    border directive done

--- src/app/core/directives/border.directive.ts
+++ src/app/core/directives/border.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appBorder]'
+  selector: '[appBorder]',
 })
 export class BorderDirective {
+  private domElement!: HTMLElement;
 
-  constructor() { }
-
+  constructor(private elementRef: ElementRef) {
+    this.domElement = this.elementRef.nativeElement;
+    this.domElement.classList.add('border-b-[1px]', 'border-gray-300');
+  }
 }
